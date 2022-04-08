0b703812fa70a189513bafb1b7d1d281ab654ba5
Isaac de Lyra Jr <isaac.lyra.junior@gmail.com>
2022-04-08 09:00:44 -0300

    home page finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
-
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Inicio | ig.news </title>
+        <title> Home | ig.news </title>
       </Head>
 
-      <h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋🏻 Hey, welcome</span>
+          <h1> News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
 
+          <SubscribeButton />
+        </section>
 
-        Hello <span> World</span>
-      </h1>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }

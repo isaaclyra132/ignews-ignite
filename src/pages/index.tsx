345bb48c4758f1d9d94aa852b75c4f6196b3a5bf
Isaac de Lyra Jr <isaac.lyra.junior@gmail.com>
2022-04-08 08:04:35 -0300

    header finished

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title> Inicio | ig.news </title>
+      </Head>
+
+      <h1>
+
+
+        Hello <span> World</span>
+      </h1>
+    </>
+  )
+}

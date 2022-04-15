b4f9ccb8aba71b5d2980dffa6266b20423e56eb3
Isaac de Lyra Jr <isaac.lyra.junior@gmail.com>
2022-04-15 08:01:33 -0300

    payment system with stripe finished

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,43 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const {data:session} = useSession();
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+            stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
     return (
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
